f9025030ffd0a63952a53efa47a4d93aee2d5eae
MoffatMore <mofenyimoffat@gmail.com>
2018-06-20 14:46:55 +0200

    fixing all tests

--- tp_screening/tests/test_eligibility.py
+++ tp_screening/tests/test_eligibility.py
@@ -6,6 +6,7 @@ Created on Jun 18, 2018
 from edc_base.utils import get_utcnow
 from ..eligibility import Eligibility, EligibilityError
 from django.test import TestCase, tag
+import copy
 
 
 class TestEligibility(TestCase):
@@ -27,4 +28,27 @@ class TestEligibility(TestCase):
 
     @tag('without_criteria')
     def test_eligibility_without_criteria(self):
-        self.assertRaises(TypeError,Eligibility)
+        self.assertRaises(TypeError, Eligibility)
+
+    def test_eligibility_with_criteria(self):
+        eligibility = Eligibility(**self.evaluator_criteria)
+        self.assertTrue(eligibility.eligible)
+        self.assertEqual(eligibility.reasons_ineligible, {'none': 'None'})
+
+    def test_eligibility_not_ok_by_age(self):
+        self.evaluator_criteria.update(age_in_years=17)
+        self.evaluator_criteria.update(guardian_present=False)
+        eligibility = Eligibility(**self.evaluator_criteria)
+        self.assertFalse(eligibility.eligible)
+        self.assertEqual(
+            eligibility.reasons_ineligible, {'minor':
+                                     'The subject is a minor and '
+                                     'No legal guardian available.'})
+
+    def test_not_eligible(self):
+        criteria = copy(self.evaluator_criteria)
+        criteria.update({'literate': False})
+        criteria.update({'literate_witness_available': False})
+        obj = Eligibility(**criteria)
+        self.assertFalse(obj.eligible)
+        self.assertIn('literate', obj.reasons_ineligible)
