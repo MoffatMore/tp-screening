b9932669a692f25d9ec4848cd82025558bb44fe0
MoffatMore <mofenyimoffat@gmail.com>
2018-06-25 15:33:58 +0200

    finilising form validators

--- tp_screening/tests/test_subject_screeing_form.py
+++ tp_screening/tests/test_subject_screeing_form.py
@@ -5,22 +5,21 @@ Created on Jun 22, 2018
 '''
 
 from django.test import TestCase, tag
+from edc_constants.constants import YES, FEMALE, NO, NOT_APPLICABLE
 from ..forms import SubjectScreeningForm
-from edc_constants.constants import YES, FEMALE, NO, NOT_APPLICABLE, MALE
 from edc_base.utils import get_utcnow
 from copy import copy
 
 
+@tag('TestSubjectForm')
 class TestSubjectScreeningForm(TestCase):
-    """Subject Screening form test"""
 
-    @tag('form_ok')
-    def test_subject_screen_form_ok(self):
-        form = dict(
-            screening_identifier='11111',
+    def setUp(self):
+        self.screening_data = dict(
+            subject_identifier='12345',
             report_datetime=get_utcnow(),
-            gender=MALE,
-            age_in_years=18,
+            gender=FEMALE,
+            age_in_years=23,
             guardian_present=NOT_APPLICABLE,
             citizen=YES,
             married_to_citizen=NOT_APPLICABLE,
@@ -30,5 +29,43 @@ class TestSubjectScreeningForm(TestCase):
             consent_ability=YES,
             consented=YES,
             reasons_ineligible={'None'})
-        subject_form = SubjectScreeningForm(data=form)
 
+    def test_default_ok(self):
+        form = SubjectScreeningForm(data=self.screening_data)
+        form.is_valid()
+        self.assertEqual(form.errors, {})
+        self.assertTrue(form.save())
+
+    def test_citizen_married_to_citizen_not_applicable(self):
+        """test when a participant is a citizen and
+           is_married_citizen is not applicable"""
+        data = copy(self.screening_data)
+        data.update(
+            citizen=YES,
+            married_to_citizen=YES,
+            marriage_certificate_present=YES)
+        form = SubjectScreeningForm(data=data)
+        form.is_valid()
+        self.assertEqual(
+            form.errors, {'married_to_citizen':
+                          ['This field is not applicable']})
+
+    def test_not_literate_witness_not_applicable(self):
+        data = copy(self.screening_data)
+        data.update(
+            literate=NO)
+        form = SubjectScreeningForm(data=data)
+        form.is_valid()
+        self.assertEqual(
+            form.errors, {'literate_witness_present':
+                          ['This field is applicable']})
+
+    def test_is_minor_guardian_none(self):
+        data = copy(self.screening_data)
+        data.update(
+            age_in_years=8)
+        form = SubjectScreeningForm(data=data)
+        form.is_valid()
+        self.assertEqual(
+            form.errors, {'guardian_present':
+                          ['This field is required.']})
